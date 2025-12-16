947962b367439003a3c8a49df20f5009cfee8af2
Keshav Sharma <Keshavasharma442@gmail.com>
2025-12-16 12:04:06 +0530

    main

--- admin/404.php
+++ admin/404.php
@@ -1,16 +1,49 @@
+<?php
+// Send proper 404 HTTP status
+http_response_code(404);
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <title>404 - Page Not Found</title>
-    <link rel="stylesheet" href="/admin/css/404.css">
+  <meta charset="UTF-8">
+  <title>404 - Page Not Found</title>
+
+  <!-- Google Font -->
+  <link href="https://fonts.googleapis.com/css?family=Arvo" rel="stylesheet">
+
+  <!-- Bootstrap -->
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.7/css/bootstrap.min.css">
+
+  <!-- 404 Page CSS -->
+  <link rel="stylesheet" href="/css/404.css">
 </head>
+
 <body>
-    <div class="container">
-        <h1>404</h1>
-        <p>Oops! Page not found.</p>
-        <img src="/admin/assets/images/icons/bg.gif" alt="404 Animation">
-        <a href="/admin/dashboard/index.html" class="btn">Go Home</a>
+
+<section class="page_404">
+  <div class="container">
+    <div class="row">
+      <div class="col-sm-12">
+        <div class="col-sm-10 col-sm-offset-1 text-center">
+
+          <div class="four_zero_four_bg">
+            <h1 class="text-center">404</h1>
+          </div>
+
+          <div class="contant_box_404">
+            <h3 class="h2">Look like you're lost</h3>
+            <p>The page you are looking for is not available.</p>
+
+            <a href="/admin/dashboard/index.html" class="link_404">
+              Go to Dashboard
+            </a>
+          </div>
+
+        </div>
+      </div>
     </div>
+  </div>
+</section>
+
 </body>
 </html>
