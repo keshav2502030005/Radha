007c91fb1067fad7383052839f9e97e14583f690
Keshav Sharma <Keshavasharma442@gmail.com>
2025-12-16 12:02:35 +0530

    main

--- admin/404.php
+++ admin/404.php
@@ -3,14 +3,14 @@
 <head>
     <meta charset="UTF-8">
     <title>404 - Page Not Found</title>
-    <link rel="stylesheet" href="css/404.css">
+    <link rel="stylesheet" href="/admin/css/404.css">
 </head>
 <body>
     <div class="container">
         <h1>404</h1>
         <p>Oops! Page not found.</p>
-        <img src="assets/images/icons/bg.gif" alt="404 Animation" class="gif">
-        <a href="dashboard/index.html" class="btn">Go Home</a>
+        <img src="/admin/assets/images/icons/bg.gif" alt="404 Animation">
+        <a href="/admin/dashboard/index.html" class="btn">Go Home</a>
     </div>
 </body>
 </html>
