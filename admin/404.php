9b67adce65d06e69c0ef65dfcfe38ce90d11ffdb
keshav2502030005 <keshavasharma23112002@gmail.com>
2025-12-17 14:36:39 +0530

    Add files via upload

--- admin/404.php
+++ admin/404.php
@@ -1,33 +1,12 @@
-
-<?php
-// Send proper 404 HTTP status
-http_response_code(404);
-?>
+<?php http_response_code(404); ?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
   <meta charset="UTF-8">
   <title>404 - Page Not Found</title>
 
-  <!-- Google Font -->
-  <link href="https://fonts.googleapis.com/css?family=Arvo" rel="stylesheet">
-
-  <!-- Bootstrap -->
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.7/css/bootstrap.min.css">
-
-  <!-- ✅ Correct CSS path -->
-  <link rel="stylesheet" href="/admin/css/404.css">
-</head>
-
-  <title>Animated 404 | @coding.stella</title>
-
-  <!-- Bootstrap CSS -->
-  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.7/css/bootstrap.min.css">
-
-  <!-- Google Fonts -->
   <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Arvo">
-
-  <!-- Custom 404 CSS -->
   <link rel="stylesheet" href="404.css">
 </head>
 <body>
@@ -35,30 +14,16 @@ http_response_code(404);
 <section class="page_404">
   <div class="container">
     <div class="row">
-      <div class="col-sm-12 text-center">
-
-        <div class="four_zero_four_bg"></div>
-
-        <div class="contant_box_404">
-          <h3 class="h2">Look like you're lost</h3>
-          <p>The page you are looking for is not available.</p>
-
-          <a href="/admin/dashboard/index.html" class="link_404">
-            Go to Dashboard
-          </a>
-        </div>
-
       <div class="col-sm-12">
         <div class="col-sm-10 col-sm-offset-1 text-center">
 
           <div class="four_zero_four_bg">
-            <h1 class="text-center">404</h1>
+            <h1>404</h1>
           </div>
 
           <div class="contant_box_404">
             <h3 class="h2">Look like you're lost</h3>
-            <p>The page you are looking for is not available!</p>
-
+            <p>The page you are looking for is not available.</p>
             <a href="/admin/dashboard/index.html" class="link_404">Go to Home</a>
           </div>
 
@@ -68,9 +33,5 @@ http_response_code(404);
   </div>
 </section>
 
-<<<<<<< HEAD
->>>>>>> feature-otp
-=======
->>>>>>> feature-otp
 </body>
 </html>
